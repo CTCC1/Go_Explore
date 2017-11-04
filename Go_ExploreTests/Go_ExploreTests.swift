1631b314c2977db386bdef20eef6e113114949b0
CTCC1 <twcnnj030@gmail.com>
2017-11-04 05:07:57 -0500

    Initial Commit

---
+++ Go_ExploreTests/Go_ExploreTests.swift
@@ -0,0 +1,36 @@
+//
+//  Go_ExploreTests.swift
+//  Go_ExploreTests
+//
+//  Created by Liu Cao on 11/4/17.
+//  Copyright © 2017 Icosahedron. All rights reserved.
+//
+
+import XCTest
+@testable import Go_Explore
+
+class Go_ExploreTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
